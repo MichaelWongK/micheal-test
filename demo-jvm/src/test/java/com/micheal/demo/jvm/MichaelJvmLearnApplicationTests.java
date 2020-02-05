90f0e6d94240d667892c51265d68df8ff9188a1f
MichaelWongK <michael.won007@gmail.com>
2020-02-05 23:47:39 +0800

    feat: init

---
+++ demo-jvm/src/test/java/com/micheal/demo/jvm/MichaelJvmLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micheal.demo.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MichaelJvmLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
