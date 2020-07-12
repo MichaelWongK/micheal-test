cfc93b1a1fd9046a0d5e4fdff5bea332452f3bc2
micheal.wang <wangmk13@163.com>
2020-07-12 14:59:55 +0800

    feat: demo for java related

---
+++ redis-demo/src/main/java/com/micheal/wang/redisdemo/service/SecKillService.java
@@ -0,0 +1,29 @@
+package com.micheal.wang.redisdemo.service;
+
+import com.micheal.wang.redisdemo.util.RedisLock;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 模拟秒杀
+ */
+public class SecKillService {
+
+    @Autowired
+    RedisLock redisLock;
+
+    //超时时间10s
+    private static final int TIMEOUT = 10 * 1000;
+
+    public void secKill(String productId) throws RuntimeException {
+        long time = System.currentTimeMillis() + TIMEOUT;
+        //加锁
+        if (!redisLock.lock(productId, String.valueOf(time))){
+            throw new RuntimeException("人太多了，等会儿再试吧~");
+        }
+
+        //具体的秒杀逻辑
+
+        //解锁
+        redisLock.unlock(productId, String.valueOf(time));
+    }
+}
---
+++ redis-demo/src/main/java/com/micheal/wang/redisdemo/util/RedisUtils.java
@@ -0,0 +1,7 @@
+package com.micheal.wang.redisdemo.util;
+
+public class RedisUtils {
+    public static void main(String[] args) {
+
+    }
+}
